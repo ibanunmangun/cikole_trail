c8f34dbbe16a35498ed7cf938e4935d5a1d07dce
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-03-24 21:11:17 +0800

    Add files via upload

---
+++ data/Pointofinterestxpublicfacilities_2.js
@@ -0,0 +1,19 @@
+var json_Pointofinterestxpublicfacilities_2 = {
+"type": "FeatureCollection",
+"name": "Pointofinterestxpublicfacilities_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "No": "1", "Destinatio": "Wisata Off Road Cikole", "Latitude": -6.7829744287535689, "Longitude": 107.64969310105111, "Info": "Point of interest" }, "geometry": { "type": "Point", "coordinates": [ 107.649693101051113, -6.782974428753569 ] } },
+{ "type": "Feature", "properties": { "No": "2", "Destinatio": "Healing Forest", "Latitude": -6.7836862941200442, "Longitude": 107.6519236, "Info": "Point of interest" }, "geometry": { "type": "Point", "coordinates": [ 107.6519236, -6.783686294120044 ] } },
+{ "type": "Feature", "properties": { "No": "3", "Destinatio": "Hutan Pinus", "Latitude": -6.779590216, "Longitude": 107.6458441, "Info": "Point of interest" }, "geometry": { "type": "Point", "coordinates": [ 107.6458441, -6.779590216 ] } },
+{ "type": "Feature", "properties": { "No": "4", "Destinatio": "Orchid Forest", "Latitude": -6.780290573, "Longitude": 107.6381724, "Info": "Point of interest" }, "geometry": { "type": "Point", "coordinates": [ 107.6381724, -6.780290573 ] } },
+{ "type": "Feature", "properties": { "No": "5", "Destinatio": "Jungle Milk", "Latitude": -6.776369004, "Longitude": 107.6249863, "Info": "Point of interest" }, "geometry": { "type": "Point", "coordinates": [ 107.6249863, -6.776369004 ] } },
+{ "type": "Feature", "properties": { "No": "6", "Destinatio": "Area Parkir 1", "Latitude": -6.782464, "Longitude": 107.650532, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.650532, -6.782464 ] } },
+{ "type": "Feature", "properties": { "No": "7", "Destinatio": "Pusat Informasi Wisata 1", "Latitude": -6.782339, "Longitude": 107.650315, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.650315, -6.782339 ] } },
+{ "type": "Feature", "properties": { "No": "8", "Destinatio": "Musholla 1", "Latitude": -6.782589363, "Longitude": 107.6501675, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.6501675, -6.782589363 ] } },
+{ "type": "Feature", "properties": { "No": "9", "Destinatio": "Cottage dan Camping Ground", "Latitude": -6.783998, "Longitude": 107.649434, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.649434, -6.783998 ] } },
+{ "type": "Feature", "properties": { "No": "10", "Destinatio": "Musholla 2", "Latitude": -6.783157, "Longitude": 107.649742, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.649742, -6.783157 ] } },
+{ "type": "Feature", "properties": { "No": "11", "Destinatio": "Area Parkir 2", "Latitude": -6.7832, "Longitude": 107.65015, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.65015, -6.7832 ] } },
+{ "type": "Feature", "properties": { "No": "12", "Destinatio": "Pusat Informasi Wisata 2", "Latitude": -6.783051, "Longitude": 107.649823, "Info": "Public Facilities" }, "geometry": { "type": "Point", "coordinates": [ 107.649823, -6.783051 ] } }
+]
+}
